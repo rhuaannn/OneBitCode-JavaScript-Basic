8549400bee005d5c1a7af9bbf20100d0272120e4
rhuaannn <rhuanmarques1223@gmail.com>
2022-12-31 17:09:05 -0300

    add to while

---
+++ while/script.js
@@ -0,0 +1,33 @@
+let name = prompt("Digite seu nome");
+
+while (name == "") {
+    alert("Nome é necessário")
+    name = prompt("Digite seu nome");
+   
+}
+ 
+
+let city = prompt("Você já visitou alguma cidade?\n 1: Sim \n 2: Nao");
+alert("Opção inválida.")
+city = prompt("Você já visitou alguma cidade?\n 1: Sim \n 2: Nao");
+
+while(city ==2){
+    alert("Precisa visitar alguma cidade")
+    city =  prompt("Você já visitou alguma cidade?\n 1: Sim \n 2: Nao");
+}
+
+let visit =""
+
+
+    
+while (city == 1) {
+   
+    city =  prompt("Você já visitou alguma cidade?\n 1: Sim \n 2: Nao");
+    visit++  
+}
+alert(`Seu nome é ${ name } e visitou um total de ${ visit } cidades`)
+alert("Software encerrado!")
+     
+        
+
+       
