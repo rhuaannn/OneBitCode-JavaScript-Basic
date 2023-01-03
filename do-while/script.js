d061594ce7b6634bef527e466129c833bd8bb477
rhuaannn <rhuanmarques1223@gmail.com>
2023-01-03 01:05:17 -0300

    add project js

---
+++ do-while/script.js
@@ -0,0 +1,12 @@
+let vision = ''
+
+do {
+ alert("Escolha uma opção do menu: \n1 - Opção-A \n2 - Opção-B \n3 - Opção-C \n4 - Opçaõ - Encerrar");
+ vision = prompt("Escolha uma opção do menu: \n1 - Opção-A \n2 - Opção-B \n3 - Opção-C \n4 - Opçaõ - Encerrar");
+} while (vision != 4)
+  {
+    alert("Software Encerrado.")
+  }
+
+
+  
