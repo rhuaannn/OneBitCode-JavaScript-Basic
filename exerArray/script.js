da25ecdb748fd9def995016abdca4e26ce4e4961
rhuaannn <rhuanmarques1223@gmail.com>
2023-01-15 21:59:40 -0300

    ex-onebitcode

--- exerArray/script.js
+++ exerArray/script.js
@@ -3,23 +3,26 @@ let name = "";
 let menu = prompt(
 "Escolha uma das OPÇÕES: \n 1- Cadastrar Paciente \n 2- Consultar Paciente \n 3- Remover Paciente \n 4- Verificar Disponibilidade \n 5- Sair");      
   
-
+  
 while (menu == 1) {
   name = prompt("Digite o nome do paciente?")
-    name = fila.push(name)
+    fila.push(name)
       console.log(fila)
         menu = prompt(
           "Escolha uma das OPÇÕES: \n 1- Cadastrar Paciente \n 2- Consultar Paciente \n 3- Remover Paciente \n 4- Verificar Disponibilidade \n 5- Sair");
   switch (menu) {
-            
+    case '2':
+      alert(`Os pacientes cadastrado são ${fila}`)
+      menu = prompt(
+        "Escolha uma das OPÇÕES: \n 1- Cadastrar Paciente \n 2- Consultar Paciente \n 3- Remover Paciente \n 4- Verificar Disponibilidade \n 5- Sair");
+      break;
     case '3':
       const lastElement = fila.slice(-1)[0]
        fila.pop(name)
-         alert(`Ultimo paciente removido: ${lastElement} sua senha é ${fila.length} `)
-          console.log(fila)
+        alert(`Paciente removido: ${lastElement}`)
           menu = prompt(
             "Escolha uma das OPÇÕES: \n 1- Cadastrar Paciente \n 2- Consultar Paciente \n 3- Remover Paciente \n 4- Verificar Disponibilidade \n 5- Sair");
-    break;
+      break;
     case '5':
 
       alert(`Software encerrado!
@@ -27,6 +30,6 @@ Os pacientes são: ${fila} `)
 break;
          
      
-    }};
+ }};
     
       
