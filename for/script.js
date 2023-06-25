46f0e1ab020b6afc7caca714f725297134591e30
rhuaannn <rhuanmarques1223@gmail.com>
2023-06-25 12:15:26 -0300

    feat: try catch js

--- for/script.js
+++ for/script.js
@@ -1,7 +1,10 @@
 let number = parseFloat(prompt("Escolha um numero para ser multiplicado?"));
-let multiplicacao = 1
-for (number = number; multiplicacao <= 20; multiplicacao++) {
-   
-    resultado = number * multiplicacao
-    alert(`A multiplicação de:${number} * ${multiplicacao} \n ${resultado}\n`)
+let operation = 1
+ 
+
+for (number; operation <= 10; operation++) {
+  result = number * multiplicacao;
+  // console.log(result)
+//  alert(`A multiplicação de:\n${number} X ${multiplicacao} \n ${result}\n`);
 }
+console.log(result)
